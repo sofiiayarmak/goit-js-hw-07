735d86eb5d2a77770bfeab936ae9e7d42d033863
Sofiia <sophia.yarmak@gmail.com>
2026-03-12 15:03:53 +0100

    fourth task

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  const formData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(formData);
+  form.reset();
+});
