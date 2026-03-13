01f9da7f47193958bae8943d56b7d3d2057b5952
Sofiia <sophia.yarmak@gmail.com>
2026-03-13 17:49:36 +0100

    task five and six

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.body;
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+
+  spanColor.textContent = newColor;
+});
